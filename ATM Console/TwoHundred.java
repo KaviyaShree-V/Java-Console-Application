cdd59d44dec638b1201eddbcb2d71953cbd6013b
KaviyaShree-V <karthikavi1976@gmail.com>
2024-12-30 20:25:10 +0530

    TwoHundred.java

--- ATM Console/TwoHundred.java
+++ ATM Console/TwoHundred.java
@@ -1,7 +1,6 @@
 public class TwoHundred extends Notes{
 
     public TwoHundred(int note, int count){
-        this.setNote(note);
-        this.setCount(count);
+        super(note,count);
     }
 }
