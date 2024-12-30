6f3e8adfeb4eb9081904293cea8a47e3d3410db8
KaviyaShree-V <karthikavi1976@gmail.com>
2024-12-30 20:25:55 +0530

    User.java

--- ATM Console/User.java
+++ ATM Console/User.java
@@ -4,19 +4,13 @@ public class User {
     private String userName ;
     private  String userPin ;
     private  double balance ;
-    private int count500;
-    private int count200;
-    private int count100;
     private static ArrayList<Transactions> transactionHistory;
-    public User(String userName , String userPin){
+    public User(String userName , String userPin , double balance){
         this.userName = userName;
         this.userPin = userPin;
-        this.balance=1500.0;
-        this.count500 = 0;
-        this.count200 = 0;
-        this.count100 = 0;
+        this.balance = balance;
         this.transactionHistory = new ArrayList<>();
-        this.transactionHistory.add(new Transactions(getId(),"Initial Amount",balance));
+        this.transactionHistory.add(new Transactions(userName,"Initial Amount", balance));
     }
 
 
@@ -44,40 +38,8 @@ public class User {
         this.userPin = newPin;
     }
 
-    public ArrayList<Transactions> getTransactionHistory() {
+    public static ArrayList<Transactions> getTransactionHistory() {
         return transactionHistory;
     }
-
-//    public void addTransaction(String transaction) {
-//        transactionHistory.add(transaction);
-//    }
-
-    public void addTransaction(String name, String transType, double transAmount) {
-        Transactions transaction = new Transactions(name, transType, transAmount);
-        transactionHistory.add(transaction);
-    }
-
-    public int getCount500() {
-        return count500;
-    }
-
-    public void setCount500(int count500) {
-        this.count500 = count500;
-    }
-
-    public int getCount200() {
-        return count200;
-    }
-
-    public void setCount200(int count200) {
-        this.count200 = count200;
-    }
-
-    public int getCount100() {
-        return count100;
-    }
-
-    public void setCount100(int count100) {
-        this.count100 = count100;
-    }
+    
 }
