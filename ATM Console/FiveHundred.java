4b640c5b47b9b024a9b8a9303113320ca0e5eed4
KaviyaShree-V <karthikavi1976@gmail.com>
2025-01-01 21:46:54 +0530

    FiveHundred.java

--- ATM Console/FiveHundred.java
+++ ATM Console/FiveHundred.java
@@ -1,6 +1,7 @@
 public class FiveHundred extends Notes{
 
+    //constructor with note and count parameters
     public FiveHundred(int note, int count){
-        super(note,count);
+        super(note,count);// call the constructor of the parent class (Notes) with the note and count arguments.
     }
 }
