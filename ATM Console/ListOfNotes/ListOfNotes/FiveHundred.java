838c327d832646bbf09e651e65665e7e47964523
KaviyaShree-V <karthikavi1976@gmail.com>
2025-03-31 23:22:02 +0530

    Updated

---
+++ ATM Console/ListOfNotes/ListOfNotes/FiveHundred.java
@@ -0,0 +1,11 @@
+package ListOfNotes;
+
+import Notes.Notes;
+
+public class FiveHundred extends Notes {
+
+    //constructor with note and count parameters
+    public FiveHundred(int note, int count){
+        super(note,count);// call the constructor of the parent class (Notes.Notes) with the note and count arguments.
+    }
+}
---
+++ ATM Console/ListOfNotes/ListOfNotes/TwoThousand.java
@@ -0,0 +1,12 @@
+package ListOfNotes;
+
+import Notes.Notes;
+
+public class TwoThousand extends Notes {
+
+    //constructor with note and count parameters
+        public TwoThousand(int note, int count){
+            super(note,count);// call the constructor of the parent class (Notes.Notes) with the note and count arguments.
+        }
+    }
+
